b9326800221fa994844eeb2c39fa19f76264f148
Anuj Tomar <anujsinghtomar111@gmail.com>
2024-08-04 01:12:36 +0530

    War Create

--- src/main/java/com/akgroup/controllers/CommonController.java
+++ src/main/java/com/akgroup/controllers/CommonController.java
@@ -1,25 +1,26 @@
 package com.akgroup.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
 	
-	@GetMapping("/index")
+	@RequestMapping("/index")
 	public String index() {
 		return "index";
 	}
-	@GetMapping("/header")
-	public String header() {
-		return "header";
-	}
-	@GetMapping("/footer")
-	public String footer() {
-		return "footer";
-	}
-	@GetMapping("/fragments")
-	public String fragments() {
-		return "fragments";
-	}
+//	@GetMapping("/header")
+//	public String header() {
+//		return "header";
+//	}
+//	@GetMapping("/footer")
+//	public String footer() {
+//		return "footer";
+//	}
+//	@GetMapping("/fragments")
+//	public String fragments() {
+//		return "fragments";
+//	}
 }
